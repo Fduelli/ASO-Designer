99fbf3b3a44c00636806899dfaabeb9555c63cfa
FDuelli <duelli.fox@gmail.com>
2025-02-04 20:37:56 -0800

    went back to before, weird issues

--- script.js
+++ script.js
@@ -1,24 +1,15 @@
 function editSequence() {
-    let sequenceInputElement = document.getElementById('sequenceInput');
-    if (!sequenceInputElement) {
-        console.error("Element with ID 'sequenceInput' not found.");
-        return;
-    }
-
-    let sequenceInput = sequenceInputElement.value.toUpperCase();
-    console.log("Input value:", sequenceInput); // Log the input value for debugging
-
-    // The rest of your logic...
-
+    let sequenceInput = document.getElementById('sequenceInput').value.toUpperCase();
+    // Add logic to modify the sequence
     let ideSeq = "";
-    let length = sequenceInput.length;
-    for(let i = 0; i < length; i++) {
-        if (i == 0) {
-            ideSeq += "/52MOEr" + sequenceInput.substring(i, i + 1) + "/*";
-        } else if (i == length - 1) {
-            ideSeq += "/32MOEr" + sequenceInput.substring(i, i + 1) + "/";
+    let length = sequenceInput.length
+    for(let i = 0; i <length; i++) {
+        if (i==0) {
+            ideSeq += "/52MOEr" + sequenceInput.substring(i, i+1) + "/*";
+        } else if (i==length-1) {
+            ideSeq += "/32MOEr" + sequenceInput.substring(i, i+1) + "/";
         } else {
-            ideSeq += "/i2MOEr" + sequenceInput.substring(i, i + 1) + "/*";
+            ideSeq += "/i2MOEr" + sequenceInput.substring(i, i+1) + "/*";
         }
     }
 
