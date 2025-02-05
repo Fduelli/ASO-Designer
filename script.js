88f468068e7ac14088c963a863f9546db98046fd
FDuelli <duelli.fox@gmail.com>
2025-02-04 19:48:48 -0800

    edited javascript

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 function editSequence() {
     let sequenceInput = document.getElementById('sequenceInput').value.toUpperCase();
     // Add logic to modify the sequence
-    let output = "";
+    let ideSeq = "";
     for(let i = 0; i <sequenceInput.length(); i++) {
         if (i==0) {
-            output += "/52MOEr" + sequenceInput.substring(i, i+1) + "/*";
+            ideSeq += "/52MOEr" + sequenceInput.substring(i, i+1) + "/*";
         } else if (i==sequenceInput.length()-1) {
-            output+= "/32MOEr" + sequenceInput.substring(i, i+1) + "/";
+            ideSeq += "/32MOEr" + sequenceInput.substring(i, i+1) + "/";
         } else {
-            output+= "/i2MOEr" + sequenceInput.substring(i, i+1) + "/*";
+            ideSeq += "/i2MOEr" + sequenceInput.substring(i, i+1) + "/*";
         }
     }
     // Display the edited sequence
-    document.getElementById('output').innerText = `IDE annotated Sequence: ${output}`;
+    document.getElementById('output').innerText = `IDE annotated Sequence: ${ideSeq}`;
 }
