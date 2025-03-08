1a7bde61ea130bfade1fc72b60eb719758d18d14
FDuelli <duelli.fox@gmail.com>
2025-03-07 16:03:03 -0800

    Added logic to sequences for Gapmer and 2MOE

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
+
+
 function editSequence() {
+    let type = document.getElementyById('dropdownMenuButton').text;
+    if(type.substring(0,1).equals("2")) {
+        editSequenceMOE();
+    }else if (type.susbtring(0,1).equals("G")) {
+        editSequenceGapmer();
+    }
+}
+
+function editSequenceMOE() {
     let sequenceInput = document.getElementById('sequenceInput').value.toUpperCase();
     // Add logic to modify the sequence
     let ideSeq = "";
@@ -15,3 +26,35 @@ function editSequence() {
 
     document.getElementById('output').innerText = `IDE Annotated Sequence: ${ideSeq}`;
 }
+
+function editSequenceGapmer() {
+    let sequenceInput = document.getElementById('sequenceInput').value.toUpperCase();
+    // Add logic to modify the sequence
+    let ideSeq = "";
+    let length = sequenceInput.length
+    for(let i = 0; i <length; i++) {
+        if(i<5 || i > length-5) {
+            if (i==0) {
+                ideSeq += "/52MOEr" + sequenceInput.substring(i, i+1) + "/*";
+            } else if (i==length-1) {
+                ideSeq += "/32MOEr" + sequenceInput.substring(i, i+1) + "/";
+            } else {
+                ideSeq += "/i2MOEr" + sequenceInput.substring(i, i+1) + "/*";
+            }
+        }else {
+            ideSeq += sequenceInput.substring(i, i+1) + "*";
+        }
+    }
+}
+
+function initializeDropdown() {
+    $('.dropdown-item').on('click', function() {
+        var selectedText = $(this).text();
+        $('#dropdownMenuButton').text(selectedText);
+    });
+}
+
+// Ensure the DOM is fully loaded before executing the function
+$(document).ready(function() {
+    initializeDropdown();
+});
